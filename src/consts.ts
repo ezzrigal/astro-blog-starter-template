25adde77f17afed3160dbe7a80dbbfa91ea021f8
AkramMa <94187330+AkramMa@users.noreply.github.com>
2026-06-17 20:09:18 +0300

    feat: add initial project structure with header, footer, global styles, and RTL layout support

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = 'شركة نقل عفش بالرياض | التميز لنقل الأثاث';
+export const SITE_DESCRIPTION = 'أفضل شركة نقل عفش بالرياض تقدم خدمات فك، تركيب، تغليف، ونقل الأثاث مع الضمان بأيدي فنيين مدربين وسيارات حديثة مجهزة. احصل على تقدير سعر فوري الآن!';
